d9a4394154c80ea9f2c6f0b609505fe985e87a7b
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-09-30 23:55:55 +0600

    shop added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css'
+const Shop = () => {
+  return (
+    <div className='shop-container'>
+      <div className="products-container">
+        <h1>This is for product </h1>
+      </div>
+      <div className="cart-container">
+
+        <h1>This is for cart </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
