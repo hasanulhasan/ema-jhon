c0d24eb8335162516900dd866bccf83603c7bbb8
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-10-04 22:44:56 +0600

    price claculation working

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
+  let total = 0;
+  for (const products of cart) {
+    total = total + products.price;
+    console.log(total)
+  }
   return (
-    <div>
-
+    <div className='cart'>
       <h3>Oder Summary</h3>
       <p>You have added {cart.length} items</p>
-      <p>Total price : </p>
+      <p>Total price :${total} </p>
       <p>Total shipping: </p>
       <p>Tax: </p>
       <h3>Grand Total: </h3>
