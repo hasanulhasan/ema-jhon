917faff881f491e333c503593cf5c61092ccb61f
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-10-04 22:55:34 +0600

    tax and shipping added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,18 +2,21 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
+  console.log(cart)
   let total = 0;
+  let shipping = 0;
   for (const products of cart) {
     total = total + products.price;
-    console.log(total)
+    shipping = shipping + products.shipping;
   }
+  let tax = (total * 0.1).toFixed(2);
   return (
     <div className='cart'>
       <h3>Oder Summary</h3>
       <p>You have added {cart.length} items</p>
       <p>Total price :${total} </p>
-      <p>Total shipping: </p>
-      <p>Tax: </p>
+      <p>Total shipping: ${shipping}</p>
+      <p>Tax: ${tax}</p>
       <h3>Grand Total: </h3>
     </div>
   );
