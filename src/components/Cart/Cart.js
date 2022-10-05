577425d44daf2bd5913535523fa711e166e6f3de
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-10-05 14:19:34 +0600

    problem of cart fixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,8 +4,10 @@ import './Cart.css'
 const Cart = ({ cart }) => {
   let total = 0;
   let shipping = 0;
+  let quantity = 0;
   for (const products of cart) {
-    total = total + products.price;
+    quantity = quantity + products.quantity;
+    total = total + products.price * products.quantity;
     shipping = shipping + products.shipping;
   }
   let tax = parseFloat((total * 0.1).toFixed(2));
@@ -13,7 +15,7 @@ const Cart = ({ cart }) => {
   return (
     <div className='cart'>
       <h3>Oder Summary</h3>
-      <p>You have added {cart.length} items</p>
+      <p>You have added {quantity} items</p>
       <p>Total price :${total} </p>
       <p>Total shipping: ${shipping}</p>
       <p>Tax: ${tax}</p>
