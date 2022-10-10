c7529bd000855798fadbfe6f3928048d5533fd48
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-10-10 14:25:23 +0600

    card style enhanced

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,3 +1,5 @@
+import { faShoppingCart, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ReviewItem.css'
 
@@ -5,15 +7,15 @@ const ReviewItem = ({ pd }) => {
   const { name, price, img, shipping } = pd;
   return (
     <div className='oder-items d-flex justify-content-center'>
-      <div className="border-3 rounded-3 shadow-lg p-4 card-width d-flex mb-3" >
+      <div className="border-3 rounded-3 shadow-sm p-4 card-width d-flex mb-3" >
         <img src={img} className="card-img-top" alt="..." />
         <div className="card-body ps-3">
           <h5 className="card-title">{name}</h5>
           <p className="card-text">Price: {price}</p>
           <p className="card-text">Shipping: {shipping}</p>
         </div>
-        <div>
-          <button>Delete</button>
+        <div className='d-flex justify-content-center align-align-items-center'>
+          <button className='btn-style'><FontAwesomeIcon icon={faTrash} className='icon-style'></FontAwesomeIcon></button>
         </div>
       </div>
     </div>
